156fcdbb0b121f9c9c823fda7f51bc46a4eaa7a8
sgautier-dev <sgautier.dev@gmail.com>
2023-10-12 18:48:40 +0400

    added clientProvider

---
+++ src/components/ClientProvider.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Toaster } from 'react-hot-toast'
+export default function ClientProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  )
+}
